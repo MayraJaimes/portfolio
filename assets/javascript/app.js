bc85cf2a1085ea172dd79c0ea2a1baeff32a482b
Mayra Jaimes <mtj26@georgetown.edu>
2018-04-19 16:35:27 -0500

    changed name of the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ var projectList = [
   //   technologies: 'HTML5, CSS, Javascript, JQuery'
   // },
   {
-    name: 'RPG Fighting Game', 
+    name: 'Heroes vs Villains', 
     description: 'In this game the app displays 4 heroes and 4 enemies. The user gets to pick a hero and this will be the character they will use for the game. They then choose the first enemy to fight. To win they have to defeat all the enemies\' health points.\'', 
     link: 'https://mayrajaimes.github.io/rpg_game/', 
     github: 'https://github.com/MayraJaimes/rpg_game',
